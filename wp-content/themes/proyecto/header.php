7c607a105f0ea0a53aa08a46bdd98c6f6253c0de
Hubert Aquino <hubertaquino15@gmail.com>
2022-06-15 22:06:10 -0500

    nuevo

---
+++ wp-content/themes/proyecto/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/estilos.css">
+  <title>Fogg Tours!</title>
+</head>
+<body>
+  <header class="cabecera">
+      <div class="container">
+        <div class="row superior">
+          <div class="col-sm-4">
+            <img class="logo" src="http://ismem1.desarrollando.tech/wp-content/uploads/2022/06/Logo-1.png">
+          </div>
+          <h2 class="col-sm-4">FOGG TOURS!</h2>
+          <div class="col-sm-4">
+            <div class="flexsearch">
+              <div class="flexsearch--wrapper">
+                <form class="flexsearch--form" action="#" method="post">
+                  <div class="flexsearch--input-wrapper">
+                    <input class="flexsearch--input" type="search" placeholder="search">
+                  </div>
+                  <input class="flexsearch--submit" type="submit" value="&#10140;"/>
+                </form>
+              </div>
+            </div>  
+          </div>
+        </div>
+      </div>
+      <nav class="arriba">
+        <ul class="container">
+          <li><a href="ofertas.html">ofertas</a></li>
+          <li><a href="index.html">inicio</a></li>
+          <li><a href="nacionales.html">nacionales</a></li>
+          <li><a href="internacionales.html">internacionales</a></li>
+          <li><a href="contacto.html">contactanos</a></li>
+        </ul>
+      </nav>
+  </header>
